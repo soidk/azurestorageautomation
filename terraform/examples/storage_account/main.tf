3d386ffae509866ce3429219bbb624db5cc67787
averedm <kkaramsetty@avereusa.com>
2020-12-16 19:59:53 -0500

    updated pipeline job

--- terraform/examples/storage_account/main.tf
+++ terraform/examples/storage_account/main.tf
@@ -1,8 +1,4 @@
 provider "azurerm" {
-  client_id = "01bc024b-aa5b-4719-840c-29b7a3dff875"
-  tenant_id = "777c1949-a60f-47dc-a0d1-457fa0e2b4a6"
-  client_secret = "IOB_Bj.T5AheL18zg._ErdEfr8296f.ckC"
-  subscription_id = "cb10d8f8-978c-4bfc-8c34-d26443752202"
   features {}
 }
 
